a5fb49664967e5abc0947d6ab9084a9dc5075e8d
jaylinrose <jaylin.manuel02@gmail.com>
2023-06-05 18:58:47 -0700

    delete route complete

--- controllers/bread_controllers.js
+++ controllers/bread_controllers.js
@@ -14,10 +14,45 @@ breads.get('/', (req,res) => {
 
 module.exports = breads
 
-//SHOW 
-breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
+// NEW
+breads.get('/new', (req, res) => {
+  res.render('new')
 })
+
+// SHOW
+breads.get('/:arrayIndex', (req, res) => {
+  if (Bread[req.params.arrayIndex]) {
+  res.render('Show', {
+    bread:Bread[req.params.arrayIndex],
+    index: req.params.arrayIndex,
+  })
+
+} else {
+  res.render('404')
+}
+})
+
+// CREATE
+breads.post('/', (req, res) => {
+  if (!req.body.image) {
+    req.body.image = "https://suebeehomemaker.com/wp-content/uploads/2021/10/sliced-french-bread.jpg"
+  }
+  if(req.body.hasGluten === 'on') {
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread.push(req.body)
+  res.redirect('/breads')
+})
+
+// DELETE
+breads.delete('/:indexArray', (req, res) => {
+Bread.splice(req.params.indexArray, 1)
+res.status(303).redirect('/breads')
+})
+
+
 
 module.exports = breads
 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //DEPENDCIES
 const express = require('express')
 const app = express()
+const methodOverride = require('method-override')
 
 //CONFIGURATION
 require('dotenv').config()
@@ -8,15 +9,27 @@ const PORT = process.env.PORT
 console.log(PORT)
 
 //middleWare
+app.use(methodOverride('_method'))
+app.use(express.urlencoded ({extended: true}))
 const breadsController = require('./controllers/bread_controllers.js')
 app.use('/breads', breadsController)
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+// MIDDLEWARE
+app.use(express.static('public'))
+
+
 //ROUTES
 app.get('/', (req,res) => {
     res.send('Welcome to an Awesome App about Breads!')
 })
+
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+  })
+  
 
 //LISTEN
 app.listen(PORT, () => {
